b3c9bf4c48062152e6e9c326e5bcba1964d37a96
Miguel Angel Gordian <os.aioria@gmail.com>
2017-04-25 04:59:19 -0500

    Add heroku config files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn yomi_backend.wsgi --log-file -
--- yomi_backend/settings.py
+++ yomi_backend/settings.py
@@ -122,9 +122,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
